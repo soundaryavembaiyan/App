476be703ba081bb0830bbe9b6761c5d0eff03512
Soundarya Vembaiyan <53108412+soundaryavembaiyan@users.noreply.github.com>
2023-12-08 15:08:07 +0530

    Update createdocument.module.ts

--- lauditor/src/app/document/createdocument/createdocument.module.ts
+++ lauditor/src/app/document/createdocument/createdocument.module.ts
@@ -1,1 +1,30 @@
 
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CreateDocumentComponent, DialogBoxComponent, SectionBoxComponent, SubSection1BoxComponent, SubSection2BoxComponent, ParagraphBoxComponent, OrderedlistBoxComponent, UnorderedlistBoxComponent  } from './createdocument.component';
+
+@NgModule({
+    imports: [
+        FormsModule,
+        RouterModule,
+        ReactiveFormsModule, 
+    ],
+    declarations: [
+        CreateDocumentComponent,
+        DialogBoxComponent,
+        SectionBoxComponent,
+        SubSection1BoxComponent, 
+        SubSection2BoxComponent, 
+        ParagraphBoxComponent,
+        OrderedlistBoxComponent,
+        UnorderedlistBoxComponent
+    ],
+    exports: [
+
+    ],
+    providers: [],
+})
+export class CreateDocumentModule{
+
+}
