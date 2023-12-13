c70a6d2d665efabac0b2a0a5997fcc71570dc1fb
Soundarya Vembaiyan <53108412+soundaryavembaiyan@users.noreply.github.com>
2023-12-13 16:06:38 +0530

    Update environment.ts

--- lauditor/src/environments/environment.ts
+++ lauditor/src/environments/environment.ts
@@ -8,7 +8,10 @@ export const environment = {
   //apiUrl: 'http://localhost:9000/cors/professional',
   paymentgatway:"https://dev2.payment.digicoffer.com/",
   emailApi:"https://dev.utils.mail.digicoffer.com/api/v1",
+  doc2pdf:"https://dev.utils.doc2pdf.digicoffer.com/api/v1/doc2pdf",
+  DOC2FILE :'https://dev.utils.doc2pdf.digicoffer.com/api/v1/docfile2pdf',
   xmppDomain: 'devchat.vitacape.com',
+  lateXAPI: 'https://devapi.latex.digicoffer.com',
   product: 'lauditor',
   production: false
 };
