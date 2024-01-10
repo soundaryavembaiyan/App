aaeb4b5bb33300fcc8f4d9130859cbe089364ebd
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2024-01-10 16:02:41 +0530

    Update http.service.ts

--- lauditor/src/app/services/http.service.ts
+++ lauditor/src/app/services/http.service.ts
@@ -16,18 +16,62 @@ export class HttpService {
   api: string;
   featureapi: string;
   emailApi:string;
+  docapi:any;
+  latexdoc:any;
   public items = new Subject();
   public selectedDocs = new Subject();
   constructor(private httpClient: HttpClient) {
     this.api = environment.apiUrl,
       this.featureapi = environment.apiUrl;
       this.emailApi=environment.emailApi;
+      this.docapi = environment.doc2pdf;
+      this.latexdoc = environment.lateXAPI;
+  }
+
+  //LATEX CALL METHODS
+  //open document
+  sendGetLatexDoc(apiUrl: any) {
+    let finalUrl = this.latexdoc + apiUrl;
+    return this.httpClient.get(finalUrl, this.get_httpoptions_latex());
+  }
+  //addDoc
+  sendPostLatexRequest(apiUrl: any, data: any): Observable<Object> {
+    let finalUrl = this.latexdoc + apiUrl;
+    return this.httpClient.post(finalUrl, data, this.get_httpoptions_latex());
+  }
+
+  //updateDoc
+  sendPatchLatexRequest(apiUrl: any, data: any): Observable<Object> {
+    let finalUrl = this.latexdoc + apiUrl;
+    return this.httpClient.patch(finalUrl, data, this.get_httpoptions_latex());
+  }
+  //Getcall
+  sendGetLatexRequest(apiUrl: any): Observable<Object> {
+    let finalUrl = this.latexdoc + apiUrl;
+    return this.httpClient.get(finalUrl, this.get_httpoptions_latex());
+  }
+  //GetPDF preview
+  sendGetLatexPDFRequest(apiUrl: any) {
+    let finalUrl = this.latexdoc + apiUrl;
+    return this.httpClient.get(finalUrl, this.get_httpoptions_latex('arraybuffer'));
+  }
+  //Delete docAPI
+  sendDeleteLatexRequest(apiUrl: any) {
+    let finalUrl = this.latexdoc + apiUrl;
+    return this.httpClient.delete(finalUrl, this.get_httpoptions_latex());
   }
 
 
   sendGetRequest(apiUrl: any) {
     let finalUrl = this.api + apiUrl;
     return this.httpClient.get(finalUrl, this.get_httpoptions());
+  }
+  sendPostDocRequest(api:any,data: any) {
+    return this.httpClient.post(this.docapi,data ,{responseType: 'arraybuffer' });
+  }
+
+  sendPostDecryptRequest(api:any,data: any) {
+    return this.httpClient.post(api,data ,this.get_httpoptions('arraybuffer'));
   }
   sendGetEmailRequest(apiUrl: any) {
     let finalUrl = this.emailApi + apiUrl;
@@ -73,11 +117,24 @@ export class HttpService {
     };
     return httpOptions
   }
-  get_httpoptions() {
+  get_httpoptions(responseType?: any) {
     const httpOptions = {
       headers: new HttpHeaders({
         "Authorization": "Bearer " + localStorage.getItem('TOKEN')
-      })
+      }),
+      responseType: responseType || 'json' // Default to 'json' if responseType is not provided
+    };
+    return httpOptions
+  }
+  //LateX Authorization
+  get_httpoptions_latex(responseType?: any) {
+    const user_id = localStorage.getItem('user_id')
+    const httpOptions = {
+      headers: new HttpHeaders({
+        "Authorization": "Bearer " + localStorage.getItem('TOKEN'),
+        "Cofferid": user_id ? user_id : '',
+      }),
+      responseType: responseType || 'json' // Default to 'json' if responseType is not provided
     };
     return httpOptions
   }
