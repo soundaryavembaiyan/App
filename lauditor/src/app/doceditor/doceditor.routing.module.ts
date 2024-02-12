c9af5ae584bb147f10a2b0eb97df49c6b782d41a
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2024-02-12 17:08:05 +0530

    Update doceditor.routing.module.ts

--- lauditor/src/app/doceditor/doceditor.routing.module.ts
+++ lauditor/src/app/doceditor/doceditor.routing.module.ts
@@ -1,1 +1,23 @@
 
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DoceditorComponent, ViewDocComponent } from './doceditor.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: DoceditorComponent,
+        children: [
+            { path: '', redirectTo: '/doceditor', pathMatch: 'full' },
+            { path: '/doceditor', component: DoceditorComponent },
+            { path: '/viewdoc', component: ViewDocComponent}
+        ]
+    },
+
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class DoceditorModule { }
