8a12ad99ba2741c3f106f371798b9fb1c99c3408
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2024-07-12 11:42:14 +0530

    Update email.module.ts

--- lauditor/src/app/email/email.module.ts
+++ lauditor/src/app/email/email.module.ts
@@ -8,9 +8,16 @@ import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { ModelModule } from '../model/model.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { ConfirmationDialogComponent } from './email.component';
+import {MatRadioModule} from '@angular/material/radio';
+
 @NgModule({
   declarations: [
-    EmailComponent
+    EmailComponent,
+    ConfirmationDialogComponent
   ],
   imports: [
     RouterModule,
@@ -21,7 +28,12 @@ import { ModelModule } from '../model/model.module';
     Ng2SearchPipeModule,
     AutocompleteLibModule,
     NgxSpinnerModule,
-    ModelModule
+    ModelModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatRadioModule
+    
 
 ],
 providers: []
