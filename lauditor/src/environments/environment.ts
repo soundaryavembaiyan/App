22a6c03184ec31f5dbd2ea3c9b13555ffb0c9d63
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2024-07-15 15:48:20 +0530

    Update environment.ts

--- lauditor/src/environments/environment.ts
+++ lauditor/src/environments/environment.ts
@@ -11,7 +11,9 @@ export const environment = {
   doc2pdf:"https://dev.utils.doc2pdf.digicoffer.com/api/v1/doc2pdf",
   DOC2FILE :'https://dev.utils.doc2pdf.digicoffer.com/api/v1/docfile2pdf',
   xmppDomain: 'devchat.vitacape.com',
+  chatops:' https://dev.utils.chat.digicoffer.com',
   lateXAPI: 'https://devapi.latex.digicoffer.com',
+  AVChat:'https://dev.testavchat.digicoffer.com/',
   product: 'lauditor',
   production: false
 };
