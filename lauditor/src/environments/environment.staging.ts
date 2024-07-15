22a19f6679d5da05f511fff7eb4ec57f43fafd68
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2024-07-15 15:48:07 +0530

    Update environment.staging.ts

--- lauditor/src/environments/environment.staging.ts
+++ lauditor/src/environments/environment.staging.ts
@@ -1,10 +1,12 @@
 export const environment = {
-    apiUrl: 'https://api.staging.digicoffer.com/professional',
-    emailApi:"https://mailapi.digicoffer.com/api/v1",
-    paymentgatway:"https://staging.payment.digicoffer.com/",
-    xmppDomain: 'chat.vitacape.com',
-    product: 'lauditor',
-    production: true,
-  };
- 
-
+  apiUrl: 'https://api.staging.digicoffer.com/professional',
+  emailApi:"https://mailapi.digicoffer.com/api/v1",
+  doc2pdf:"https://dev.utils.doc2pdf.digicoffer.com/api/v1/doc2pdf",
+  DOC2FILE :'https://dev.utils.doc2pdf.digicoffer.com/api/v1/docfile2pdf',
+  chatops:' https://dev.utils.chat.digicoffer.com',
+  lateXAPI: ' https://stagingapi.latex.digicoffer.com',
+  paymentgatway:"https://staging.payment.digicoffer.com/",
+  xmppDomain: 'chat.vitacape.com',
+  product: 'lauditor',
+  production: true,
+};
