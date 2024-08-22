5211d4d697aa5504f821b7321f9a15050a7784e6
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2024-08-22 10:22:29 +0530

    Update dashboard.component.ts

--- lauditor/src/app/dashboard/dashboard.component.ts
+++ lauditor/src/app/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnChanges, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { environment } from 'src/environments/environment';
-import { AjaxService } from '../services/ajax.service';
 import { HttpService } from '../services/http.service';
+import { HttpClient } from '@angular/common/http';
 
 
 @Component({
@@ -12,11 +12,12 @@ import { HttpService } from '../services/http.service';
 })
 export class DashboardComponent implements OnInit, OnChanges {
 
-  categoryName: any = 'Dashboard';
+  categoryName:  string = "Dashboard";
   name: string = "";
   role: string = "";
   roleId: string = "TM";
   product = environment.product;
+  key:any;
 
   navItem = [
     {
@@ -36,9 +37,9 @@ export class DashboardComponent implements OnInit, OnChanges {
       id: 'documents', link: '/documents', image: 'assets/img/Document.svg', class: ''
     },
     {
-      name: 'Doc Editor',
+      name: 'Doc-Editor',
       roles: ['TM', 'GH', 'SU'],
-      id: 'doceditor', link: '/doceditor', image: 'assets/img/docedi.svg', class: ''
+      id: 'Doc-Editor', link: '/doceditor', image: 'assets/img/docedi.svg', class: ''
     },
     {
       name: 'Relationships',
@@ -115,6 +116,11 @@ export class DashboardComponent implements OnInit, OnChanges {
       id: 'documents', link: '/documents/upload/firm', image: 'assets/img/Document.svg', class: ''
     },
     {
+      name: 'Doc-Editor',
+      roles: ['TM', 'GH', 'SU'],
+      id: 'Doc-Editor', link: '/doceditor', image: 'assets/img/docedi.svg', class: ''
+    },
+    {
       name: 'Relationships',
       roles: ['TM', 'GH', 'SU'],
       id: 'relationships',
@@ -168,6 +174,11 @@ export class DashboardComponent implements OnInit, OnChanges {
       name: 'Documents',
       roles: ['TM', 'GH', 'SU'],
       id: 'documents', link: '/documents', image: 'assets/img/Document.svg', class: ''
+    },
+    {
+      name: 'Doc-Editor',
+      roles: ['TM', 'GH', 'SU'],
+      id: 'Doc-Editor', link: '/doceditor', image: 'assets/img/docedi.svg', class: ''
     },
     {
       name: 'Relationships',
@@ -291,36 +302,89 @@ export class DashboardComponent implements OnInit, OnChanges {
     }
   ]
 
-
-  constructor(private router: Router, private httpService: HttpService) {
+  constructor(private router: Router, private httpService: HttpService,private route: ActivatedRoute,
+    private httpClient: HttpClient) {
+    
     this.router.events.subscribe((val) => {
       if (val instanceof NavigationEnd) {
         //console.log("windowLoc", window.location.pathname.split("/").splice(-4)[1]);
         //this.getCategory(window.location.pathname.split("/").splice(-4)[1]);
       }
     });
+    
     // this.httpService.items.subscribe((newItem: any) => {
     //   this.getCategory(newItem);
     // });
   }
   ngOnInit() {
-    var name = localStorage.getItem("name")
-    if (name != null) { this.name = name }
-    var role = localStorage.getItem("role")
-    if (role != null) { this.roleId = role }
-    if (role == 'SU') { this.role = 'SuperUser' }
-    if (role == 'AAM') { this.role = 'Admin' }
-    if (role == 'TM') { this.role = 'Team Member' }
-    if (role == 'GH' && this.product == 'corporate') { this.role = 'Department Head' }
-    if (role == 'GH' && this.product != 'corporate') { this.role = 'Group Head' }
+    this.route.queryParams.subscribe(params => {
+      this.key = params['key'];
+      if (params['key']) {
+        console.log(params['key'])
+        const jsonString = this.key?.replace(/^data=/, '');
+        const jsonObject = JSON.parse(jsonString);
+        console.log(jsonObject)
+        this.getSOSlogin(jsonObject)
+        this.initalize_roles()
+      }
+    });
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const activeItem = this.navItem.find(item => this.router.url.includes(item.link));
+        if (activeItem) {
+          this.categoryName = activeItem.name;
+          localStorage.setItem('categoryName', this.categoryName);
+          localStorage.setItem('activeNavItem', activeItem.id);
+        } else {
+          this.categoryName = 'Dashboard';
+        }
+      }
+    });
+
+    // Set initial values from localStorage on load
+    const storedCategoryName = localStorage.getItem('categoryName');
+    if (storedCategoryName) {
+      this.categoryName = storedCategoryName;
+    }
+
+    this.initalize_roles()
   }
 
-  getCategory(category: any) {
+  initalize_roles(){
+    var name = localStorage.getItem("name")
+      if (name != null) { this.name = name }
+      var role = localStorage.getItem("role")
+      if (role != null) { this.roleId = role }
+      if (role == 'SU') { this.role = 'SuperUser' }
+      if (role == 'AAM') { this.role = 'Admin' }
+      if (role == 'TM') { this.role = 'Team Member' }
+      if (role == 'GH' && this.product == 'corporate') { this.role = 'Department Head' }
+      if (role == 'GH' && this.product != 'corporate') { this.role = 'Group Head' }
+  }
 
+  getCategory(category: string): void {
     this.categoryName = category;
     //console.log("categoryName", this.categoryName);
+    localStorage.setItem('categoryName', this.categoryName);
+    localStorage.setItem('activeNavItem', category);
+    // const categoryList = document.getElementsByClassName("left-menu-icon");
+    // for (let i = 0; i < categoryList.length; i++) {
+    //   if (categoryList[i].classList.contains(category)) {
+    //     categoryList[i].classList.add('active');
+    //   } else {
+    //     categoryList[i].classList.remove('active');
+    //   }
+    // }
+  }
 
-    const categoryList = document.getElementsByClassName("left-menu-icon");
+  isActive(itemId: string): boolean {
+    return localStorage.getItem('activeNavItem') === itemId;
+  }
+
+  setActiveNavItem(category: string) {
+    //console.log("settoGetcategoryName", this.categoryName);
+    const categoryList = document.getElementsByClassName('left-menu-icon');
     for (let i = 0; i < categoryList.length; i++) {
       if (categoryList[i].classList.contains(category)) {
         categoryList[i].classList.add('active');
@@ -329,11 +393,11 @@ export class DashboardComponent implements OnInit, OnChanges {
       }
     }
   }
+
   ngOnChanges() {
     // this.httpService.items.subscribe((newItem: any) => {
     //   this.getCategory(newItem);
     // });
-
   }
 
   logout() {
@@ -345,6 +409,15 @@ export class DashboardComponent implements OnInit, OnChanges {
     this.router.navigate(['/'])
     localStorage.clear();
     window.location.reload();
+  }
+  getSOSlogin(resp:any){
+    localStorage.setItem('TOKEN', resp['token'])
+        localStorage.setItem('name', resp['data']['name'])
+        localStorage.setItem('role', resp['data']['role'])
+        localStorage.setItem('isadmin', resp['data']['admin'])
+        localStorage.setItem('jid', resp['data']['uid'])
+        localStorage.setItem('firm_name', resp['data']['firm_name'])
+
   }
   //   getButtonActive(buttonName:any){
   //     const categoryList=document.getElementsByClassName("left-menu-icon");
